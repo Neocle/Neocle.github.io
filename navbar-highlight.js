87f926ad1150c600cdea716920e075788252272e
Neocle <137337301+Neocle@users.noreply.github.com>
2025-04-13 22:11:34 +0200

    Delete navbar-highlight.js

--- navbar-highlight.js
+++
@@ -1,7 +0,0 @@
-const currentPath = window.location.pathname;
-
-document.querySelectorAll('.navbar a').forEach(link => {
-  if (link.getAttribute('href') === currentPath) {
-    link.classList.add('active');
-  }
-});

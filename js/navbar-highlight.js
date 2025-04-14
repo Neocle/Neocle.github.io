3214de8a88c30bc59f1d5e25ba12250c6b53ccd2
unknown <lilian433700@gmail.com>
2025-04-14 14:02:56 +0200

    fixed navbar highlighting

--- js/navbar-highlight.js
+++ js/navbar-highlight.js
@@ -1,11 +1,17 @@
-const currentPath = window.location.pathname.replace(/\/$/, '') || '/';
+const normalizePath = (path) => {
+  return path
+    .replace(/\/$/, '')
+    .replace(/\.html$/, '')
+    || '/';
+};
+
+const currentPath = normalizePath(window.location.pathname);
 
 document.querySelectorAll('.navbar a').forEach(link => {
   const href = link.getAttribute('href');
-
   if (!href || href === '#') return;
 
-  const normalizedHref = href.length > 1 ? href.replace(/\/$/, '') : href;
+  const normalizedHref = normalizePath(href);
 
   if (normalizedHref === currentPath) {
     link.classList.add('active');
